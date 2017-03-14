9015d59e5f049c832b064babdfd617530ceb6f33
FedorArbuzovHSE <ArbuzovFP@yandex.ru>
2017-03-14 11:13:02 +0300

    task 2 and task 3

--- Task3/task3_py.py
+++ Task3/task3_py.py
@@ -4,15 +4,15 @@ import matplotlib.pyplot as plt
 
 data = pd.read_excel('Данные к задаче 3 дз.xls')
 
-x_data = list(data['x1'])
-y_data = list(data.loc[1])
+x_data = list(data['x9'])
+y_data = list(data['y9'])
 
 
-print(stat.pearsonr(x_data[:20], y_data)[0])
+print(stat.pearsonr(x_data, y_data)[0])
 
-print(stat.spearmanr(x_data[:20], y_data)[0])
+print(stat.spearmanr(x_data, y_data)[0])
 
-plt.scatter(x_data[:20], y_data)
+plt.scatter(x_data, y_data)
 plt.show()
 
 
